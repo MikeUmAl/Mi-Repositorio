f7f7b0ef15b78d2e85dfdc5366caaac00bc32629
MikeUmAl <85909317+MikeUmAl@users.noreply.github.com>
2021-06-30 19:22:58 -0600

    Actualizacion

---
+++ Practica 3G.R
@@ -0,0 +1,20 @@
+#grafico de barras
+
+#Calificacion de los documentales
+datosFiltrados = filter(NetflixOriginals, NetflixOriginals$Genre=='Documentary')
+plot(as.factor(datosFiltrados$`IMDB Score`),main = "Clasificacion de Documentales") 
+
+#Duracion de las peliculas +/- 100 minutos
+datosFiltrados = filter(NetflixOriginals, NetflixOriginals$Genre=='Documentary')
+plot(as.factor(datosFiltrados$`Runtime`>100),main = "Duracion de los documentales")
+
+#Time based plot - fechas del mes de Junio
+
+fechas <- seq(as.Date("2021/6/1"), by = "day", length = 30 ) 
+
+plot(fechas,rnorm(30), type = "o",main = "Fechas de Junio")
+
+#correlacion  relacion entre la puntuacion y la duracion
+View(NetflixOriginals)
+
+plot(select(NetflixOriginals,4:5),main = "Relacion Puntuacion/Duracion")
