8a34df6f18ac01e3a42c8dedb2117365b462d9c6
MikeUmAl <85909317+MikeUmAl@users.noreply.github.com>
2021-07-14 20:25:26 -0600

    actualizacion

--- Practica 5.R
+++ Practica 5.R
@@ -10,3 +10,8 @@ Regresion = lm(AtaqueCardiaco~Edad, data = AtaqueCardiaco)
 summary(Regresion)
 
 #muestra, variables, que obtuvimos 
+#Edad con presión 0.27
+
+#Edad con colesterol 0.21
+
+#Edad con latidos -0.39 
