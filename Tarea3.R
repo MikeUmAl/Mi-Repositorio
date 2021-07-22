85570027d94d8b37f6ed0ee39d52e8869f973a20
MikeUmAl <85909317+MikeUmAl@users.noreply.github.com>
2021-07-21 18:00:39 -0600

    actualizacion

--- Tarea3.R
+++ Tarea3.R
@@ -64,6 +64,56 @@ predict(Regresion3,nuevoindicerenta)
 Data3 = predict(Regresion3,nuevoindicerenta)
 plot(Data3)
 
+#analisis de sentimiento
+#Syuzhet
+
+# Instala los paquetes:
+install.packages("syuzhet")
+install.packages("RColorBrewer")
+install.packages("wordcloud")
+install.packages("tm")
+
+# Carga los paquetes
+library(syuzhet)
+library(RColorBrewer)
+library(wordcloud)
+library(tm)
+
+#Paso 1 crear texto 
+
+texto_cadena ='Retumbó el disparo en la soledad de aquel abandonado y tenebroso lugar,
+dando terrible salto, hincó la cabeza en la movediza tierra y rodó seco 
+hacia el abismo, sin que el conocimiento le durase más que el tiempo
+necesario para poder decir: «Pues… sí…»'
+
+#Paso 2 tokenizar
+texto_palabras <- get_tokens(texto_cadena)
+head(texto_palabras)
+length(texto_palabras)
+
+#Paso 3 crear oraciones test
+oraciones_vector <- get_sentences(texto_cadena)
+length(oraciones_vector)
+
+#Paso 4 Extracción de datos con el Léxico de Sentimientos NRC
+
+sentimientos_df <- get_nrc_sentiment(texto_palabras, lang="spanish")
+
+View(sentimientos_df)
+summary(sentimientos_df)
+
+#graficar
+barplot(
+  colSums(prop.table(sentimientos_df[,1:8])),
+  space = 0.2,
+  horiz = FALSE,
+  las = 1,
+  cex.names = 0.7,
+  col = brewer.pal(n = 8,  name = "Set3"),
+  main = "Situacion",
+  sun = "Analisis",
+  xlab = "emociones", lab = NULL)
+
 
 
 
